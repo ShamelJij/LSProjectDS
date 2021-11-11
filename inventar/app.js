adeefe712a84b7ca928ce93b8579ccc23ff9c959
shameljij <shamel.jij@deltastone@com>
2021-11-11 16:45:59 +0100

    js added

---
+++ inventar/app.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+
+
+
+document.getElementById("parag").innerHTML =  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, assumenda atque aut cumque doloribus earum eos error explicabo facere illum impedit in porro praesentium quaerat quia tempora ut. Dignissimos exercitationem fugiat tempora? Ab animi, cum, doloremque doloribus ducimus enim ex excepturi harum id nam natus nobis nostrum officiis perferendis placeat quam quidem quo repudiandae similique voluptatem! Doloribus expedita quaerat sit?";
