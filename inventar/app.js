50c48ee72c6c9b4a4dac8b62e41eab3165f1250e
shameljij <shamel.jij@deltastone@com>
2021-11-16 10:30:37 +0100

    fixed css

--- inventar/app.js
+++ inventar/app.js
@@ -1,10 +1,2 @@
 
-
-
-
-
-
-
-
-
-document.getElementById("parag").innerHTML =  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, assumenda atque aut cumque doloribus earum eos error explicabo facere illum impedit in porro praesentium quaerat quia tempora ut. Dignissimos exercitationem fugiat tempora? Ab animi, cum, doloremque doloribus ducimus enim ex excepturi harum id nam natus nobis nostrum officiis perferendis placeat quam quidem quo repudiandae similique voluptatem! Doloribus expedita quaerat sit?";
+document.getElementById("parag").innerHTML =  "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. <b> parag parag parag parag</b> Alias, assumenda atque aut cumque doloribus earum eos error explicabo facere illum impedit in porro praesentium quaerat quia tempora ut. Dignissimos exercitationem fugiat tempora? Ab animi, cum, doloremque doloribus ducimus enim ex excepturi harum id nam natus nobis nostrum officiis perferendis placeat quam quidem quo repudiandae similique voluptatem! Doloribus expedita quaerat sit?";
