4354f5a2f9d8a1e0ba5c445a2b37aa064a4d83d7
shameljij <shamel.jij@gmail.com>
2021-11-16 18:06:10 +0100

    array added

--- DOMProject/LSProject/app.js
+++ DOMProject/LSProject/app.js
@@ -1,25 +1,21 @@
 //JavaScript for LSProject
-let myObj = {
-    name: "dominic",
-    age: 32
-};
-let myObj_serialized= JSON.stringify(myObj);
-console.log(myObj_serialized);
-localStorage.setItem("myObj", myObj);
-console.log(localStorage);
-let myObj_deserialized = localStorage.getItem("myObj");
-console.log(myObj_deserialized);
+
+
 function save(){
 
     var new_data = document.getElementById('input').value;
     if(localStorage.getItem('data') == null){
-        localStorage.getItem('data','[]');
+        localStorage.setItem('data','[]');
 
     }
-    //test
-
+    var old_data = JSON.parse(localStorage.getItem('data'));
+    old_data.push(new_data);
+    localStorage.setItem('data', JSON.stringify(old_data));
 }
-function view(){
+    function view(){
+        if (localStorage.getItem('data')!=null){
+            document.getElementById('output').innerHTML = JSON.parse(localStorage.getItem('data'));
+        }
 }
 
 
