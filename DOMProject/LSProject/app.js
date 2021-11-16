db2ec8a4daa521e69309fe0aeb7127aeb545ad70
shameljij <shamel.jij@gmail.com>
2021-11-16 16:59:47 +0100

    local stroage funtionadded

--- DOMProject/LSProject/app.js
+++ DOMProject/LSProject/app.js
@@ -1,1 +1,21 @@
 //JavaScript for LSProject
+let myObj = {
+    name: "dominic",
+    age: 32
+};
+let myObj_serialized= JSON.stringify(myObj);
+console.log(myObj_serialized);
+localStorage.setItem("myObj", myObj);
+console.log(localStorage);
+let myObj_deserialized = localStorage.getItem("myObj");
+console.log(myObj_deserialized);
+function save(){
+    if(localStorage)
+
+}
+function view(){
+
+}
+
+
+
